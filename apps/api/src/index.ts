ec1c3a2af815feb9a7d070a506c8ab8e9ed606c0
Satrio Wicaksono <satwica69@gmail.com>
2026-03-19 14:43:57 +0700

    refactor(api): reorganize main server entry in index.ts

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,68 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.use(logger());
+
+app.use(
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowHeaders: ["Authorization", "Content-Type", "User-Agent"],
+    exposeHeaders: [
+      "Content-Length",
+      "Content-Type",
+      "Cache-Control",
+      "Cross-Origin-Resource-Policy",
+    ],
+    maxAge: 86400,
+  })
+);
+
+app.use(prettyJSON());
+
+app.get("/health", (c) => {
+  return c.json(
+    {
+      message: "OK",
+    },
+    200
+  );
+});
+
+app.notFound((c) => {
+  return c.json(
+    {
+      message: "Not Found",
+    },
+    404
+  );
+});
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    console.error(err);
+
+    return c.json(
+      {
+        message: err.message,
+      },
+      err.status
+    );
+  }
+
+  console.error(err);
+
+  return c.json(
+    {
+      message: "Internal Server Error",
+    },
+    500
+  );
 });
 
 export default app;
