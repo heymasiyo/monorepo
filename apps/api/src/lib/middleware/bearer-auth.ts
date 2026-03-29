ab30c35cc1da8da588d5690640c7a416230df61d
Satrio Wicaksono <satwica69@gmail.com>
2026-03-29 08:50:32 +0700

    feat(api): add jwt bearer auth middleware
    - implement custom bearer auth middleware using JWT

---
+++ apps/api/src/lib/middleware/bearer-auth.ts
@@ -0,0 +1,21 @@
+import type { Bindings } from "@/lib/types/bindings";
+import type { Context } from "hono";
+
+import { bearerAuth } from "hono/bearer-auth";
+import { verify } from "hono/jwt";
+
+export function withBearerAuth() {
+  return bearerAuth({
+    verifyToken: async (token, c: Context<{ Bindings: Bindings }>) => {
+      try {
+        const payload = await verify(token, c.env.JWT_SECRET, "HS256");
+
+        c.set("jwtPayload", payload);
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  });
+}
